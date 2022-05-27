5a4186bd9f655b2570b8960852c414fdaadf9fa7
Katsiaryna Pryshchepava <kpryshchepava@gomel.iba.by>
2022-05-27 16:03:29 +0300

    task8

---
+++ Task8/Task8/Program.cs
@@ -0,0 +1,11 @@
+﻿class Task8
+{
+    public static void Main()
+    {
+        Console.WriteLine("Add some text:\n");
+        List<string> text = new List<string>();
+        text.Add(Console.ReadLine());
+        TextTransformer.Show(text);
+
+    }
+}
---
+++ Task8/Task8/TextTransformer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+
+public class TextTransformer
+{
+	public static IEnumerable Transformer(List<string> str)
+    {
+        if (str == null) yield return "This is null value";
+        foreach (var letter in str)
+        {
+            yield return letter.ToUpper();
+        }
+    }
+
+    public static void Show(List<string> str)
+    {
+        foreach (var text in Transformer(str)) Console.WriteLine(text);
+    }
+}

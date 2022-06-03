3ac04955bcc7b8cb133c00baea04b0722efb85e2
Katsiaryna Pryshchepava <kpryshchepava@gomel.iba.by>
2022-06-03 10:30:32 +0300

    task9

--- Task8/Task8/Program.cs
+++ Task8/Task8/Program.cs
@@ -1,7 +1,12 @@
-﻿class Task8
+﻿using System.Globalization;
+
+class Task8
 {
     public static void Main()
     {
+        DateTime dt = new DateTime();
+
+        Console.WriteLine(dt.ToString("d", new CultureInfo("fr-FR")));
         Console.WriteLine("Add some text:\n");
         List<string> text = new List<string>();
         text.Add(Console.ReadLine());
---
+++ Task9/Task9/Program.cs
@@ -0,0 +1,45 @@
+﻿class Task9 {
+    public static DirectoryInfo DirectoryInfo;
+    public static string DirectoryPath = @"D:\Pryshchepava\IBA\C# 5.0 and .Net Course\C-Sharp-Course\Task9\testFolder";
+    public static void ListFiles(string directory)
+    {
+        DirectoryInfo = new DirectoryInfo(directory);
+
+        try
+        {
+            var i = 0;
+            foreach (FileInfo fileInfo in DirectoryInfo.GetFiles())
+            {
+                i++;
+                Console.WriteLine("{0}: {1}", i, fileInfo.Name);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Some problem!");
+            return;
+        }
+        
+    }
+
+    public static void GetFile()
+    {
+        var value = Console.ReadLine();
+        var selectedNumber = int.Parse(value) - 1;
+        if (selectedNumber == null || selectedNumber > DirectoryInfo.GetFiles().Length || selectedNumber < 0)
+        {
+            Console.WriteLine("Select valid number!");
+        } 
+        else {
+            Console.WriteLine(File.ReadAllText(Path.Combine(DirectoryPath, DirectoryInfo.GetFiles()[selectedNumber].Name)));
+        }
+    }
+
+
+    static void Main()
+    {
+        ListFiles(DirectoryPath);
+        Console.WriteLine("\nPlease, select some file from the list\n");
+        GetFile();
+    }
+}
